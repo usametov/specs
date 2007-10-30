1e2bf8b9b01db081b31bca6a00a278b180c084b8
etorreborre <etorreborre@40e86570-fb37-0410-8f9f-bd8ce7e8790c>
2007-10-30 15:10:10 +0000

    made a copy

---
+++ branches/SPECS-1.0/src/main/scala/scala/specs/matcher/NumericMatchers.scala
@@ -0,0 +1,41 @@
+package scala.specs.matcher
+
+/**
+ * The <code>NumericMatchers</code> trait provides matchers which allow numerical comparisons
+ */
+trait NumericMatchers {
+  /**
+   * Matches if x < n
+   */   
+  def beStrictlyLessThan[S <% Double](n: S) = new Matcher[S](){ 
+     def apply(x: => S) = (x < n, x + " is strictly less than " + n, x + " is not strictly less than " + n)
+   }
+
+  /**
+   * Matches if x > n
+   */   
+  def beStrictlyGreaterThan[S <% Double](n: S) = new Matcher[S](){ 
+     def apply(x: => S) = (x > n, x + " is strictly greater than " + n, x + " is not strictly greater than " + n)
+   }
+
+  /**
+   * Matches if x <= n
+   */   
+  def beLessThan[S <% Double](n: S) = new Matcher[S](){ 
+     def apply(x: => S) = (x <= n, x + " is less than " + n, x + " is not less than " + n)
+   }
+
+  /**
+   * Matches if x >= n
+   */   
+  def beGreaterThan[S <% Double](n: S) = new Matcher[S](){ 
+     def apply(x: => S) = (x >= n, x + " is greater than " + n, x + " is not greater than " + n)
+   }
+
+  /**
+   * Matches if x = n +/- delta
+   */   
+  def beCloseTo[S <% Double](n: S, delta: S) = new Matcher[S](){ 
+     def apply(x: => S) = ((n - delta <= x) && (x <= n + delta), x + " is close " + n + " +/- " + delta, x + " is not close " + n + " +/- " + delta)
+   }
+}
