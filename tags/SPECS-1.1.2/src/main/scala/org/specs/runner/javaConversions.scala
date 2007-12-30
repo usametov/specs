fd08060c17ad74c5c04cf8a54e88162ac5fef973
etorreborre <etorreborre@40e86570-fb37-0410-8f9f-bd8ce7e8790c>
2007-12-30 14:51:42 +0000

    made a copy

---
+++ tags/SPECS-1.1.2/src/main/scala/org/specs/runner/javaConversions.scala
@@ -0,0 +1,4 @@
+package org.specs.runner
+
+object javaConversions {
+}
---
+++ tags/SPECS-1.1.2/src/test/scala/org/specs/matcher/allMatchersSpec.scala
@@ -0,0 +1,30 @@
+package org.specs.matcher
+import scala.collection.mutable.Queue
+import org.specs.runner._
+import org.specs.Sugar._
+import org.specs.specification._
+
+object allMatchersTestSuite extends JUnit3(matchersSpec) 
+object matchersSpec extends Specification {
+  "Matchers" areSpecifiedBy (objectMatchersSpec, 
+                             stringMatchersSpec, 
+                             iterableMatchersSpec, 
+                             mapMatchersSpec,
+                             patternMatchersSpec,
+                             scalacheckMatchersSpec)
+}
+class MatchersSpecification extends Specification {
+  var reported: Example = new Example("this example serves as a stub to collect failure messages", new Sut("", this))
+  def clearExample = { reported = new Example("", new Sut("", this)) }
+  def failWith(message: String) = is_==(message)
+  def failWithMatch(pattern: String) = beMatching(pattern)
+  def assertion(value: => Any): String = {
+    try {
+      value
+    } catch {
+      case FailureException(message) => return message
+      case t: Throwable => throw t
+    }
+    return "this assertion has not failed"
+  }
+}
