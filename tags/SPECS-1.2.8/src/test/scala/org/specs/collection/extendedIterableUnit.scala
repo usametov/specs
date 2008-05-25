60eadd13f5e978ba9403c10d66acdd190e202963
etorreborre <etorreborre@40e86570-fb37-0410-8f9f-bd8ce7e8790c>
2008-05-25 06:48:08 +0000

    made a copy

---
+++ tags/SPECS-1.2.8/src/test/scala/org/specs/collection/extendedIterableUnit.scala
@@ -0,0 +1,35 @@
+package org.specs.collection
+import org.specs.runner._
+import org.specs.Sugar._
+import org.specs._
+import org.specs.collection.ExtendedIterable._
+import org.specs.collection.ExtendedList._
+import scalacheck.Gen._
+import org.specs.Scalacheck
+
+class extendedIterableUnitTest extends Runner(extendedIterableUnit) with JUnit
+object extendedIterableUnit extends Specification with Sugar with Scalacheck {
+  "A toDeepString function" should {
+    "print the inside of an iterable, even if it is a Stream" in {
+      Stream.cons(1, Stream.cons(2, Nil.toStream)).toDeepString must_== "[1, 2]"
+    }
+  }
+  "A sameElementsAs function" should {
+    val sameIterables = for (i1 <- listOf(elements(1, 2, 3, listOf(elements(1, 2, 3))));
+                             val i2 = i1.scramble)
+                          yield (i1, i2)
+    "return true if the 2 iterables are the same" in {
+      sameIterables must pass { t: (Iterable[Any], Iterable[Any]) => val (i1, i2) = t
+        i1 must haveSameElementsAs(i2)
+      }
+    }
+    "return true if the 2 iterables are the same, even with a stream and a list" in {
+      val sameIterables = for (i1 <- listOf(elements(1, 2, 3, listOf(elements(1, 2, 3)).toStream));
+                            val i2 = i1.scramble.toList)
+                            yield (i1.toStream, i2)
+      sameIterables must pass { t: (Iterable[Any], Iterable[Any]) => val (i1, i2) = t
+        i1 must haveSameElementsAs(i2)
+      }
+    }
+  }
+}
---
+++ tags/SPECS-1.2.8/src/test/scala/org/specs/specification/beforeAfterSpec.scala
@@ -0,0 +1,164 @@
+package org.specs.specification
+import org.specs.io.mock.MockOutput
+import org.specs.runner._
+import org.specs._
+import org.specs.runner._
+
+class beforeAfterTest extends Runner(beforeAfterSpec) with JUnit 
+object beforeAfterSpec extends Specification {
+  "A specification with before clauses" should {
+    "have each example using the doBefore method before being executed" in { 
+      doBeforeExample.execute
+      doBeforeExample.messages mustContain "before called 1"
+      doBeforeExample.messages mustContain "before called 2"
+    } 
+    "not execute its test if the doBefore method fails" in { 
+      doBeforeExampleFailing.execute
+      doBeforeExampleFailing.messages must existMatch("1 error")
+      doBeforeExampleFailing.messages must notExistMatch("tested")
+    }
+    "throw an exception if the doBefore clause is not declared inside a sut" in { 
+      object badSpec extends Specification {
+        doBefore {}
+      }
+      badSpec must throwA(new FailureException(""))
+    } 
+    "deprecated - have each example using the usingBefore method before being executed" in { 
+      beforeEx.execute
+      beforeEx.messages mustContain "before called 1"
+      beforeEx.messages mustContain "before called 2"
+    } 
+    "deprecated - not execute its test if the usingBefore method fails" in { 
+      beforeExampleFailing.execute
+      beforeExampleFailing.messages must existMatch("1 error")
+      beforeExampleFailing.messages must notExistMatch("tested")
+    } 
+    "deprecated - throw an exception if the usingBefore clause is not declared inside a sut" in { 
+      object badSpec extends Specification {
+        usingBefore {() => ()}
+      }
+      badSpec must throwA(new FailureException(""))
+    } 
+  }
+  "A specification with after clauses" should {
+    "have each example using the doAfter method after being executed" in { 
+      doAfterExample.execute
+      doAfterExample.messages mustContain "after called 1"
+      doAfterExample.messages mustContain "after called 2"
+    } 
+    "not execute its test if the doAfter method fails" in { 
+      doAfterExampleFailing.execute
+      doAfterExampleFailing.messages must existMatch("1 error")
+      doAfterExampleFailing.messages must notExistMatch("tested")
+    }
+    "throw an exception if the doAfter clause is not declared inside a sut" in { 
+      object badSpec extends Specification {
+        doAfter {}
+      }
+      badSpec must throwA(new FailureException(""))
+    } 
+    "deprecated - have each example using the usingAfter method after being executed" in { 
+      afterEx.execute
+      afterEx.messages mustContain "after called 1"
+      afterEx.messages mustContain "after called 2"
+    } 
+    "deprecated - not execute its test if the usingAfter method fails" in { 
+      afterExampleFailing.execute
+      afterExampleFailing.messages must existMatch("1 error")
+      afterExampleFailing.messages must notExistMatch("tested")
+    } 
+    "deprecated - throw an exception if the usingAfter clause is not declared inside a sut" in { 
+      object badSpec extends Specification {
+        usingAfter {() => ()}
+      }
+      badSpec must throwA(new FailureException(""))
+    } 
+  }
+}
+
+trait beforeAfterTestSpec extends Specification with ConsoleReporter with MockOutput {
+  def execute = { suts = Nil; executeSpec }
+  def executeSpec
+}
+
+object doBeforeExample extends beforeAfterTestSpec {
+  override def executeSpec = {
+    var beforeCalls = 0
+    "A specification" should { doBefore { beforeCalls += 1; println("before called " + beforeCalls) }
+      "have example 1 ok" in { true }
+      "have example 2 ok" in { true }
+    }
+    reportSpec(this)
+  }   
+}
+object doBeforeExampleFailing extends beforeAfterTestSpec {
+  override def executeSpec = {
+      var beforeCalls = 0
+    "A specification" should { doBefore { error("before error") }
+      "have example 1 ok" in {  }
+    }
+    reportSpec(this)
+  }   
+}
+object beforeEx extends beforeAfterTestSpec {
+  override def executeSpec = {
+    "A specification" should {
+      var beforeCalls = 0
+      usingBefore { () => beforeCalls += 1; println("before called " + beforeCalls) }
+      "have example 1 ok" in { true }
+      "have example 2 ok" in { true }
+    }
+    reportSpec(this)
+  }   
+}
+object beforeExampleFailing extends beforeAfterTestSpec {
+  override def executeSpec = {
+    "A specification" should {
+      var beforeCalls = 0
+      usingBefore { () => error("before error") }
+      "have example 1 ok" in { }
+    }
+    reportSpec(this)
+  }   
+}
+object doAfterExample extends beforeAfterTestSpec {
+  override def executeSpec = {
+    var afterCalls = 0
+    "A specification" should { doAfter { afterCalls += 1; println("after called " + afterCalls) }
+      "have example 1 ok" in { true }
+      "have example 2 ok" in { true }
+    }
+    reportSpec(this)
+  }   
+}
+object doAfterExampleFailing extends beforeAfterTestSpec {
+  override def executeSpec = {
+      var afterCalls = 0
+    "A specification" should { doAfter { println("after");error("after error") }
+      "have example 1 ok" in {  }
+    }
+    reportSpec(this)
+  }   
+}
+object afterEx extends beforeAfterTestSpec {
+  override def executeSpec = {
+    "A specification" should {
+      var afterCalls = 0
+      usingAfter { () => afterCalls += 1; println("after called " + afterCalls) }
+      "have example 1 ok" in { true }
+      "have example 2 ok" in { true }
+    }
+    reportSpec(this)
+  }   
+}
+object afterExampleFailing extends beforeAfterTestSpec {
+  override def executeSpec = {
+    "A specification" should {
+      var afterCalls = 0
+      usingAfter { () => error("after error") }
+      "have example 1 ok" in { }
+    }
+    reportSpec(this)
+  }   
+}
+
