4dea2b4b12c609c6cb06c6d9121d7489e54f3358
etorreborre <etorreborre@40e86570-fb37-0410-8f9f-bd8ce7e8790c>
2008-05-27 01:23:56 +0000

    made a copy

---
+++ tags/SPECS-1.2.9/src/test/scala/org/specs/util/allUtil.scala
@@ -0,0 +1,21 @@
+package org.specs.util
+
+import org.specs.runner._
+import org.specs._
+
+
+class allUtilTest extends JUnit4(allUtilSpec)
+object allUtilSpec extends Specification {
+    "The util specifications" areSpecifiedBy (
+        timerSpec,
+        ExtendedStringSpec
+    )
+}
+
+object allUtilUnit extends Specification {
+  "The unit tests for the util package" areSpecifiedBy (
+      dataRowUnit, 
+      dataTableHeaderUnit,  
+      dataTableUnit)
+}
+
