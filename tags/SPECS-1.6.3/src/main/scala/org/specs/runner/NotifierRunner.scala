70b56ffb4e23f85c85f5d28f18b0117ca7323b04
etorreborre <etorreborre@40e86570-fb37-0410-8f9f-bd8ce7e8790c>
2010-02-04 21:24:21 +0000

    made a copy

---
+++ tags/SPECS-1.6.3/src/main/scala/org/specs/runner/NotifierRunner.scala
@@ -0,0 +1,120 @@
+/**
+ * Copyright (c) 2007-2009 Eric Torreborre <etorreborre@yahoo.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software. Neither the name of specs nor the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written permission.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+package org.specs.runner
+import org.specs.specification._
+import org.specs._
+import org.specs.util.LazyParameter
+
+/**
+ * This is a generic trait for defining a notified object which will know about the state of a run.
+ */
+trait Notifier {
+  def runStarting(examplesCount: Int)
+  def exampleStarting(exampleName: String)
+  def exampleSucceeded(testName: String)
+  def exampleFailed(testName: String, e: Throwable)
+  def exampleError(testName: String, e: Throwable)
+  def exampleSkipped(testName: String)
+  def systemStarting(systemName: String)
+  def systemSucceeded(name: String)
+  def systemFailed(name: String, e: Throwable)
+  def systemError(name: String, e: Throwable)
+  def systemSkipped(name: String)
+  def systemCompleted(systemName: String)
+}
+/**
+ * This reporter reports specification by executing them and specifying the results to Notifiers.
+ */
+class NotifierRunner(val specifications: Array[Specification], val notifiers: Array[Notifier]) extends Reporter {
+  val specs = specifications.toList
+  def this(s: Specification, n: Notifier) = this(Array(s), Array(n))
+  override def report(specs: Seq[Specification]): this.type = {
+    super.report(specs)
+    val specToRun = if (specs.size == 1)
+                      specs(0)
+                    else {
+                      object totalSpecification extends Specification { include(specs.map(s => new LazyParameter(() => s)):_*) }
+                      totalSpecification
+                    }
+    
+    notifiers.foreach { _.runStarting(specToRun.firstLevelExamplesNb) } 
+    reportASpecification(specToRun)
+  }
+  def reportASpecification(spec: Specification): this.type = {
+    notifiers.foreach { _.systemStarting(spec.description) }
+    for (subSpec <- spec.subSpecifications) {
+      reportASpecification(subSpec)
+    }
+    for (system <- spec.systems)
+      reportSystem(system)
+    notifiers.foreach { _.systemCompleted(spec.description) }
+    this
+  }
+  def reportSystem(system: Sus): this.type = {
+    notifiers.foreach { _.systemStarting(system.header) }
+    
+    if (!system.ownFailures.isEmpty)
+      notifiers.foreach { notifier =>
+        system.ownFailures.foreach { failure =>
+          notifier.systemFailed(system.description, failure) 
+        }
+      }
+    else if (!system.ownErrors.isEmpty)
+      notifiers.foreach { notifier =>
+        system.ownErrors.foreach { error =>
+          notifier.systemError(system.description, error) 
+        }
+      }
+    else if (!system.ownSkipped.isEmpty)
+      notifiers.foreach { notifier =>
+        system.ownSkipped.foreach { skipped =>
+          notifier.systemSkipped(skipped.getMessage)
+        }
+      }
+    for (example <- system.examples)
+      reportExample(example)
+    notifiers.foreach { _.systemCompleted(system.header) }
+    this
+  }
+  def reportExample(example: Examples): this.type = {
+    notifiers.foreach { _.exampleStarting(example.description) }
+    
+    example.examples.foreach(reportExample(_))
+    if (example.isOk)
+      notifiers.foreach { _.exampleSucceeded(example.description) }
+    else if (!example.failures.isEmpty)
+      notifiers.foreach { notifier =>
+        example.failures.foreach { failure =>
+          notifier.exampleFailed(example.description, failure) 
+        }
+      }
+    else if (!example.errors.isEmpty)
+      notifiers.foreach { notifier =>
+        example.errors.foreach { error =>
+          notifier.exampleError(example.description, error) 
+        }
+      }
+    else if (!example.skipped.isEmpty)
+      notifiers.foreach { notifier =>
+        notifier.exampleSkipped(example.description) 
+      }
+    this
+  }
+}
+
---
+++ tags/SPECS-1.6.3/src/test/scala/org/specs/runner/testInterfaceRunnerSpec.scala
@@ -0,0 +1,107 @@
+package org.specs.runner
+import org.spex._
+import org.scalatools.testing._
+import scala.collection.mutable._
+
+class testInterfaceRunnerSpec extends Specification {
+  "the test interface runner" should {
+    "report a sus" in {
+      logOutput must include("[info] this sus should")      
+    }
+    "report a success" in {
+      logOutput must include("[info]   + success")      
+    }
+    "report a failure" in {
+      logOutput must include("[info]   x failure")      
+    }
+    "report an error" in {
+      logOutput must include("[info]   x error")      
+    }
+    "report a skipped" in {
+      logOutput must include("[info]   o skipped")      
+    }
+    "log messages using colors if the logger supports them" in {
+      logColoredOutput must include(AnsiColors.red)      
+    }
+    "report a success event" in {
+      events must include("Success")      
+    }
+    "report a failure event" in {
+      events must include("Failure")      
+    }
+    "report an error event" in {
+      events must include("Error")      
+    }
+    "report a skipped event" in {
+      events must include("Skipped")      
+    }
+    "work ok with a specification created as an object" in {
+      logOutput$ must include("[info] this sus should")      
+    }
+    "work ok if an error is launch before any example is specified - issue 111" in {
+      logOutput("org.specs.runner.issue111Specification") must not(throwAn[Error])
+    }
+  }
+  class TestInterfaceLogger extends Logger {
+    var out = ""
+    def ansiCodesSupported = false;
+    def error(msg: String) = out += "[error] " + msg + "\n"
+    def warn(msg: String) = out += "[warn] " + msg + "\n"
+    def info(msg: String) = out += "[info] " + msg + "\n"
+    def debug(msg: String) = out += "[debug] " + msg + "\n"
+    def trace(t: Throwable) = out += "[trace] " + t.getMessage + "\n"
+  }
+  val testInterfaceLogger = new TestInterfaceLogger
+  val testInterfaceColoredLogger = new TestInterfaceLogger { override def ansiCodesSupported = true }
+  val handler = new EventHandler {
+    val events: ListBuffer[String] = new ListBuffer
+    def handle(event: Event)= events.append(event.result.toString)
+  }
+  def executeRunner: Any = executeRunner("org.specs.runner.testInterfaceSpecification")
+  def executeRunner$ = executeRunner("org.specs.runner.testInterfaceSpecification$")
+  def executeRunner(className: String): Any = new TestInterfaceRunner(getClass.getClassLoader, Array(testInterfaceLogger)).run(className, null, handler, Array())
+
+  def logOutput = {
+    executeRunner
+    testInterfaceLogger.out
+  }
+  def logOutput$ = {
+    executeRunner$
+    testInterfaceLogger.out
+  }
+  def logOutput(className: String) = {
+    executeRunner(className)
+    testInterfaceLogger.out
+  }
+  def logColoredOutput = {
+    new TestInterfaceRunner(getClass.getClassLoader, Array(testInterfaceColoredLogger)).run("org.specs.runner.testInterfaceSpecification", null, handler, Array())
+    testInterfaceColoredLogger.out
+  }
+  def events = {
+    executeRunner
+    handler.events.mkString("\n")
+  }
+}
+class testInterfaceSpecification extends Specification {
+  "this sus" should {
+    "success" in {
+      1 must_== 1
+    }
+    "failure" in {
+      1 must_== 2
+    }
+    "error" in {
+      error("bad")
+    }
+    "skipped" in {
+      skip("dont do this")
+    }
+  }
+}
+object testInterfaceSpecification extends testInterfaceSpecification
+class issue111Specification extends Specification {
+  "this sus" should {
+    throw new Error("here")
+    "an example" in { 1 must_== 1 }
+  }
+}
